1c9e0a5982cf59ce471ef00b5ff9c5c2979085c6
Joshua Manela <jmmanela@mtu.edu>
2015-05-10 15:09:17 -0400

    did things

---
+++ blink.js
@@ -0,0 +1,22 @@
+//This is the first time i've ever done javascript,
+//please don't hate :/
+window.addEventListener("load", function() {
+	var names = ["Gets", " shit", " done."];
+	var cur = 0;
+	var str = "";
+    var f = document.getElementById('Foo');
+    var timerId = setInterval(function() {
+    	var idx = cur%names.length;
+    	if (idx == 0) {
+    		str = "";
+    	}
+
+    	str = str.concat(names[idx]);
+        f.innerHTML= str;
+        cur = cur + 1;
+
+        if (idx == 2) {
+        	clearInterval(timerId);
+        }
+    }, 2000);
+}, false);
