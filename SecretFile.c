3193ab4146f4e61a1ca590712435090d4ea86fa3
Joshua Manela <jmmanela@mtu.edu>
2016-02-23 17:30:42 -0500

    more secret

--- SecretFile.c
+++ SecretFile.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    printf("Hello <INSERT NAME HERE>!");
+    printf("Hello %s!  Congrats, you are now almost able to scp a toaster.\n", getenv("USER"));
     return 0;
 }
