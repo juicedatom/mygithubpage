c63a9da2031bc2b7ee7bd3f1ea6e4e984a5d9f33
Joshua Manela <jmmanela@mtu.edu>
2016-10-09 09:29:59 -0400

    Cleaned up the blink script by adding comments and making the timer a variable which can be adjusted.

--- blink.js
+++ blink.js
@@ -1,22 +1,34 @@
-//This is the first time i've ever done javascript,
+// This is the first time i've ever done javascript,
 //please don't hate :/
 window.addEventListener("load", function() {
+
+    // Setup variables for the timer.
 	var names = ["Gets", " shit", " done", "."];
 	var cur = 0;
 	var str = "";
+    var time_to_wait_ms = 500 
+
+    // Grab the element I wish to flash.
     var f = document.getElementById('Foo');
+
+    // Create the interval timer.
     var timerId = setInterval(function() {
+
+        // Everytime we iterate through the function, append another string.
     	var idx = cur%names.length;
     	if (idx == 0) {
     		str = "";
     	}
 
+        // Perform the concatenation.
     	str = str.concat(names[idx]);
         f.innerHTML= str;
         cur = cur + 1;
 
+        // Clear the timer when we're done.
         if (idx == names.length-1) {
         	clearInterval(timerId);
         }
-    }, 500);
+    }, time_to_wait_ms);
+
 }, false);
