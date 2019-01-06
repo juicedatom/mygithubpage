9201f369d3cf290ddf7999f8b99a5fc655d27626
Joshua manela <juicedatom@gmail.com>
2019-01-06 08:42:17 -0500

    remove unused files.

--- SecretFile.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    printf("Hello %s!  Congrats, you are now almost able to scp a toaster.\n", getenv("USER"));
-    printf("I made a modification.");
-    return 0;
-}
